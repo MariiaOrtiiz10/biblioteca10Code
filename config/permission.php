14514e484864085b1854dd78b42932539f5309bf
MariiaOrtiiz10 <mariamontewei10@gmail.com>
2025-03-21 10:25:56 +0100

    .

--- config/permission.php
+++ config/permission.php
@@ -13,7 +13,7 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => \Domain\Permission\Models\Permission::class,
+        'permission' => \Domain\Permissions\Models\Permission::class,
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
